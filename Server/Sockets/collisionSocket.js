3953ca933a4b10ed0d4154fb4bb7552524551a6c
christianrevillard <christian.revillard@centraliens.net>
2014-11-30 13:28:19 +0100

    refactoring in progress.

--- Server/Sockets/collisionSocket.js
+++ Server/Sockets/collisionSocket.js
@@ -34,119 +34,101 @@ var CollisionTest = function(collision, socket){
 	this.controller = new serverController.Controller(collision, socket.id, false)
 	this.controller.addSocket(socket);	
 
-	this.controller.addElement
-	(
-		["name", "left"],
-		["image", { "width":20,"height":500, "typeName": 'wall'}],
-		["position", {"x": 10, "y": 250}],			
-		["solid", {mass:Infinity}],
-		["clickable", {}]
-	);
+	this.controller.addElement({
+		name: 'left',
+		typeName: 'wall',
+		box: {width:20, height:500 },
+		position: {x: 10, y: 250},			
+		solid: {mass:Infinity}
+	});
 
-	this.controller.addElement
-	(
-		["name", "right"],
-		["image", { "width":20,"height":500, "typeName": 'wall'}],
-		["position", {"x": 690, "y": 250}],			
-		["solid", {mass:Infinity}]
-	);
+	this.controller.addElement({
+		name: 'right',
+		typeName: 'wall',
+		box: {width:20, height:500 },
+		position: {x: 690, y: 250},			
+		solid: {mass:Infinity}
+	});
 
-	this.controller.addElement
-	(
-		["name", "top"],
-		["image", { "width":700,"height":20, "typeName": 'top'}],
-		["position", {"x": 350, "y": 10}],			
-		["solid", {mass:Infinity}]
-	);
-
-	this.controller.addElement
-	(
-		["name", "bottom"],
-		["image", { "width":700,"height":20, "typeName": 'top'}],
-		["position", {"x": 350, "y": 490}],			
-		["solid", {mass:Infinity}]
-	);
+	this.controller.addElement({
+		name: 'top',
+		typeName: 'top',
+		box: {width:700, height:20 },
+		position: {x: 350, y: 10},			
+		solid: {mass:Infinity}
+	});
+	
+	this.controller.addElement({
+		name: 'bottom',
+		typeName: 'top',
+		box: {width:700, height:20 },
+		position: {x: 350, y: 490},			
+		solid: {mass:Infinity}
+	});
 		
 	
-	for (i=2;i<27;i++)
+	for (i=10;i<15;i++)
 	{
-		this.controller.addElement
-		(
-			["name", "round1"],
-			["image", { "width":20,"height":20, "typeName": 'round'}],
-			["position", {"x": 25*i, "y": 20}],			
-			["solid", {mass:1}],
-			["clickable", {}],
-			["moving", {vy:100}]
-		);
-		
-		this.controller.addElement
-		(
-			["name", "round1"],
-			["image", { "width":20,"height":20, "typeName": 'round'}],
-			["position", {"x": 25*i, "y": 470}],			
-			["solid", {mass:1}],
-			["clickable", {}],
-			["moving", {vy:-100}]
-		);
+		this.controller.addElement({
+			name: 'round1',
+			typeName: 'round',
+			box: {width:700, height:20 },
+			position: {x: 25*i, y: 20},			
+			solid: {mass:1},
+			moving: {speed:{y:100}}
+		});
+
+		this.controller.addElement({
+			name: 'round1',
+			typeName: 'round',
+			box: {width:700, height:20 },
+			position: {x: 25*i, y: 470},			
+			solid: {mass:1},
+			moving: {speed:{y:-100}}
+		});
 	}
 
 	for (i=4;i<25;i++)
 	{
-		this.controller.addElement
-		(
-			["name", "round1"],
-			["image", { "width":20,"height":20, "typeName": 'round'}],
-			["position", {"x": 25*i+10, "y": 100}],			
-			["solid", {mass:1}],
-			["clickable", {}],
-			["moving", {vy:150}]
-		);
-		
-		this.controller.addElement
-		(
-			["name", "round1"],
-			["image", { "width":20,"height":20, "typeName": 'round'}],
-			["position", {"x": 25*i+10, "y": 350}],			
-			["solid", {mass:1}],
-			["clickable", {}],
-			["moving", {vy:-150}]
-		);
+		this.controller.addElement({
+			name: 'round1',
+			typeName: 'round',
+			box: {width:700, height:20 },
+			position: {x: 25*i+10, y: 100},			
+			solid: {mass:1},
+			moving: {speed:{y:150}}
+		});
+
+		this.controller.addElement({
+			name: 'round1',
+			typeName: 'round',
+			box: {width:700, height:20 },
+			position: {x: 25*i+10, y: 350},			
+			solid: {mass:1},
+			moving: {speed:{y:-150}}
+		});
 	}
 
 	for (i=2;i<20;i++)
 	{
-		this.controller.addElement
-		(
-			["name", "round1"],
-			["image", { "width":20,"height":20, "typeName": 'round'}],
-			["position", {"x": 20, "y": 25*i}],			
-			["solid", {mass:1}],
-			["clickable", {}],
-			["moving", {vx:100}]
-		);
+		this.controller.addElement({
+			name: 'round1',
+			typeName: 'round',
+			box: {width:700, height:20 },
+			position: {x: 20, y: 25*i},			
+			solid: {mass:1},
+			moving: {speed:{x:100}}
+		});
 
-		this.controller.addElement
-		(
-			["name", "round1"],
-			["image", { "width":20,"height":20, "typeName": 'round'}],
-			["position", {"x": 680, "y": 25*i}],			
-			["solid", {mass:1}],
-			["clickable", {}],
-			["moving", {vx:-100}]
-		);
+		this.controller.addElement({
+			name: 'round1',
+			typeName: 'round',
+			box: {width:700, height:20 },
+			position: {x: 680, y: 25*i},			
+			solid: {mass:1},
+			moving: {speed:{x:-100}}
+		});
 	}
-
-	/*
-	this.controller.addElement
-	(
-		["name", "round4"],
-		["image", { "width":150,"height":50, "typeName": 'redRound'}],
-		["position", {"x": 350, "y": 400}],			
-		["solid", {mass:5}],
-		["clickable", {}],
-		["moving", {vx:50,vy:50}]
-	);*/
 	
 	this.disconnect = function()
 	{
